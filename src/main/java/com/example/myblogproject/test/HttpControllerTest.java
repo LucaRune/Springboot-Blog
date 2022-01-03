0a12ae96acc5cfe4fe543e43d028ae24c55c6480
Sujin <tnwlsk0190@gamil.com>
2022-01-03 16:40:06 +0900

    "1월3일"

--- src/main/java/com/example/myblogproject/test/HttpControllerTest.java
+++ src/main/java/com/example/myblogproject/test/HttpControllerTest.java
@@ -6,7 +6,20 @@ import org.springframework.web.bind.annotation.*;
 //@Controller
 
 @RestController //사용자가 요청 -> 응답(Data)
+// RestController 가 String 을 리턴했을 때는 문자 그 자체를 리턴. => ex) lombok test 완료   리턴
 public class HttpControllerTest {
+
+    private static final String TAG = "HttpControllerTest : ";
+
+    //localhost:8000/blog/http/lombok
+    @GetMapping("/http/lombok")
+    public String lombokTest(){
+        Member m = new Member(1, "sujin", "1234", "email");
+        System.out.println(TAG + "getter : " + m.getId());
+        m.setId(5000);
+        System.out.println(TAG + "setter : " + m.getId());
+        return "lombok test 완료";
+    }
 
     //인터넷 브라우저 요청은 무조건 get 요정만 할 수 있다.
     //get 말고는 오류 발생
@@ -17,19 +30,26 @@ public class HttpControllerTest {
         return "get 요청 : " + id + "  username : " + username;}  //문자열을 리턴*/
 
     @GetMapping("/http/get")     //Member는 알아서 매핑
-    public String getTest(Member m){//?id=1&username=sujin&password=1234&email=su@naver.com ?뒤에껄 Member에 넣어주는 것도 스프링이 하는 일
-        return "get 요청 : " + m.getId() + m.getUsername() + m.getPassword() + m.getEmail();}
+    public String getTest(Member m){
+        //?id=1&username=sujin&password=1234&email=su@naver.com ?뒤에껄
+        // Member에 넣어주는 것도 스프링이 하는 일(MessageConverter)
+
+        return "get 요청 : " + m.getId() +","+ m.getUsername() +","+ m.getPassword() +","+ m.getEmail();}
 
     //http://localhost:8080/http/post (insert)
-    @PostMapping("/http/post")
-    public String postTest(){
-        return "post 요청";
+    /*@PostMapping("/http/post")  //text/plain
+    public String postTest(@RequestBody String text){
+        return "post 요청 : " + text;
+    }*/
+    @PostMapping("/http/post")  //application/json
+    public String postTest(@RequestBody Member m){  // MessageConverter (스프링부트)
+        return "post 요청" + m.getId() +","+ m.getUsername() +","+ m.getPassword() +","+ m.getEmail();
     }
 
     //http://localhost:8080/http/put (update)
     @PutMapping("/http/put")
-    public String putTest(){
-        return "put 요청";
+    public String putTest(@RequestBody Member m){
+        return "put 요청 : " + m.getId() +","+ m.getUsername() +","+ m.getPassword() +","+ m.getEmail();
     }
 
     //http://localhost:8080/http/delete (delete)
@@ -38,6 +58,9 @@ public class HttpControllerTest {
         return "delete 요청";
     }
 }
+
+//Body에 데이터를 실어서 보내면 스프링에서는 간단하게 오브젝트로 매핑해서 받을 수 있는데
+//RequestBody라는 어노테이션을 통해 매핑해서 받는다.
 
 //postman에서 401 오류
 //Authorization 탭 -> Basic Auth에서
@@ -60,3 +83,24 @@ where : 쿼리스트링 이름
 nexearch :쿼리스트링 값
 여러개의 이름&값은 &기호로 구분된다.
 */
+
+/* lib를 외부에 두면 장점은 파일 하나로 양쪽에서 사용할 수 있다는 점
+    단점은 배포하게 되면 또 연결해야한다는 점
+
+    파일앞에 .붙은건 숨김 파일 ex) .user
+
+    Maven은 프로젝트 관리 도구
+    pom.xml에 필요한 걸 기술 -> .m2 파일의 repository에서 빌드해서 씀
+    배포해야하는 서버에 프로젝트 그대로 넣고, maven설치만 해주면 사용 가능
+*/
+
+/* yaml 야몰파일
+king: Pod => : 다음 스페이스바 한칸
+metadata 안에 name 이면
+metadata:
+  name: hello-pod
+이렇게 괄호 대신 들여쓰기
+들여쓰기는 스페이스 두칸
+
+스프링에서는 yml 하나로 관리
+ */
